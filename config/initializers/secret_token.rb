31770814723e990e7a357b55cdfcf1037cabda92
Ken Lu <ken@seeice.com>
2012-01-20 18:13:58 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IcCommunity::Application.config.secret_token = 'bf1ababb8550ed042ac43b94e87a115244044f5dcb1c5327fe59f0b3a288e9349bad98ecf4bd57d0841d497f6e38bd643f49d769be61208fa560bb9fb54c483c'
